b84e463d3b259cff02489e18c55ba469a263a0c5
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-05 19:50:02 +0800

    Update app.py - Apply OOP

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import logging
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+class WebApp:
+    @app.route("/")
+    def index():
+        return render_template('index.html')
+    
+    def start():
+        # Logging Configuration
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)s]: %(message)s",
+            datefmt="%b-%d-%Y %I:%M %p",  # %I for 12-hour format and %p for AM/PM
+        )
+        logging.info("Web applicatin has been started!")
 
 if __name__ == "__main__":
     app.run()
