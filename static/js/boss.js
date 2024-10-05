7a05c1d09f4c28149558598d90ef44ce80fc7693
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-05 23:01:20 +0800

    Update boss.js

--- static/js/boss.js
+++ static/js/boss.js
@@ -2,11 +2,37 @@ class Boss {
   /**
    * @param {string} codeName
    * @param {string} name
-   * @param {QuizQuestion} dialogue - Array of dialogue strings.
+   * @param {QuizQuestion[]} questions - Array of QuizQuestion objects.
    */
   constructor(codeName, name, questions) {
-    this.codeName = codeName;
-    this.name = name;
-    this.questions = questions;
+    this.#codeName = codeName;
+    this.#name = name;
+    this.#questions = questions;
+  }
+
+  // Private variables
+  #codeName;
+  #name;
+  #questions;
+
+  /**
+   * @returns {string} The code name of the boss.
+   */
+  getCodeName() {
+    return this.#codeName;
+  }
+
+  /**
+   * @returns {string} The name of the boss.
+   */
+  getName() {
+    return this.#name;
+  }
+
+  /**
+   * @returns {QuizQuestion[]} The array of questions.
+   */
+  getQuestions() {
+    return this.#questions;
   }
 }
