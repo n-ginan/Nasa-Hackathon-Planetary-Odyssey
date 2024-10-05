56a1c434b5ea85933e1144bb7b1eda133515858f
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-05 20:21:17 +0800

    Update quiz_question.js - Implement type hinting

--- static/js/quiz_question.js
+++ static/js/quiz_question.js
@@ -1,4 +1,10 @@
 class QuizQuestion {
+  /**
+   * @param {string} question - The question text.
+   * @param {string} answer - The correct answer.
+   * @param {string[]} dialogue - Array of dialogue strings.
+   * @param {string[]} choices - Array of possible answer choices.
+   */
   constructor(question, answer, dialogue, choices) {
     this.question = question;
     this.correctAnswer = answer;
@@ -6,7 +12,11 @@ class QuizQuestion {
     this.choices = choices;
   }
 
-  answer(userAnswer) {
+  /**
+   * @param {string} userAnswer - The user's answer.
+   * @returns {boolean} Whether the answer is correct.
+   */
+  isAnswerCorrect(userAnswer) {
     return userAnswer === this.correctAnswer;
   }
 }
