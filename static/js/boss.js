3afbd5d825959977297775688df41edae20dedb9
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-05 20:48:27 +0800

    Create boss.js

---
+++ static/js/boss.js
@@ -0,0 +1,12 @@
+class Boss {
+  /**
+   * @param {string} codeName
+   * @param {string} name
+   * @param {QuizQuestion} dialogue - Array of dialogue strings.
+   */
+  constructor(codeName, name, questions) {
+    this.codeName = "";
+    this.name = "";
+    this.questions = [];
+  }
+}
