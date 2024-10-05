23d187085b96d07f3cfcf1eb0b4e7b9bf47d52cf
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-05 10:44:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+import logging
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+class WebApp:
+    @app.route("/")
+    def index():
+        return render_template('index.html')
+    
+    @staticmethod
+    def start():
+        # Logging Configuration
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)s]: %(message)s",
+            datefmt="%b-%d-%Y %I:%M %p",  # %I for 12-hour format and %p for AM/PM
+        )
 
 if __name__ == "__main__":
+    WebApp.start()
     app.run()
