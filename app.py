b63049140c8ca5adc402c4229acccf352825192d
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-05 20:05:51 +0800

    Implement connection from home page to loading page

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 class WebApp:
     @app.route("/")
     def index():
-        return render_template('index.html')
+        return render_template("index.html")
+    
+    @app.route("/loading")
+    def loading():
+        return render_template("loading.html")
     
     def start():
         # Logging Configuration
