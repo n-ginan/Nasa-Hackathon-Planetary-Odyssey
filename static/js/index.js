43abf8510c986120317967eb3fec46d61407b93e
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-06 09:40:47 +0800

    Fix bug with the start adventure button not working

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,3 @@
-import { TOI_1231_B } from "./quiz_data.js";
-
 document.addEventListener("DOMContentLoaded", () => {
   const startBtn = document.getElementById("startAdventureBtn");
   console.log(TOI_1231_B.getQuestions());
