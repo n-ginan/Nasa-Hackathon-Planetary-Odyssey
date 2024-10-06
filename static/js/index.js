d0c5f2b17255ee0cf653f668d592b75448c10db4
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-06 09:41:39 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const startBtn = document.getElementById("startAdventureBtn");
-  console.log(TOI_1231_B.getQuestions());
 
   startBtn.addEventListener("click", () => {
     window.location.href = "/loading";
