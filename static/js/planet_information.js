0ad9bdf7c3eaf54034af178ec8ea2a472411b633
DragunDev <plarisanmarc@gmail.com>
2024-10-06 22:45:37 +0800

    Improve planet information headers

--- static/js/planet_information.js
+++ static/js/planet_information.js
@@ -17,36 +17,42 @@ document.addEventListener("DOMContentLoaded", () => {
   const planetContent = [
     {
       code: "TOI_1231_B",
+      header: "TOI-1231 b",
       body: TOI_1231_B.getInformation(),
       imageLocation: "/static/res/TOI-1231_planet.PNG",
       destination: bossTransition("TOI_1231_B"),
     },
     {
       code: "L_98_59_D",
+      header: "wolf 1061 c",
       body: L_98_59_D.getInformation(),
       imageLocation: "/static/res/Wolf_1061_c_planet.png",
       destination: bossTransition("L_98_59_D"),
     },
     {
       code: "GJ_1002_B",
+      header: "GJ 1002 b",
       body: GJ_1002_B.getInformation(),
       imageLocation: "/static/res/GJ_1002_b_planet.png",
       destination: bossTransition("GJ_1002_B"),
     },
     {
       code: "PROXIMA_CENTAURI_B",
+      header: "Proxima Centauri b",
       body: PROXIMA_CENTAURI_B.getInformation(),
       imageLocation: "/static/res/Proxima_Centauri_b_planet.PNG",
       destination: bossTransition("PROXIMA_CENTAURI_B"),
     },
     {
       code: "EPSILON_ERIDANI_B",
+      header: "Epsilon eridani b",
       body: EPSILON_ERIDANI_B.getInformation(),
       imageLocation: "/static/res/Epsilon_Eridani_B_planet.PNG",
       destination: bossTransition("EPSILON_ERIDANI_B"),
     },
     {
       code: "GLIESE_832_C",
+      header: "Gliese 832 c",
       body: GLIESE_832_C.getInformation(),
       imageLocation: "/static/res/Gliese_832_C_planet.PNG",
       destination: bossTransition("GLIESE_832_C"),
@@ -67,7 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // Sets the information about the chosen planet
-    planetHeader.innerText = chosenPlanet.code;
+    planetHeader.innerText = chosenPlanet.header;
     planetImage.setAttribute("src", chosenPlanet.imageLocation);
     firstParagraph.innerText = chosenPlanet.body[0];
     secondParagraph.innerText = chosenPlanet.body[1];
