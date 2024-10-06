53727f649e332738697d29e5b214f12c73afcae4
DragunDev <plarisanmarc@gmail.com>
2024-10-06 21:45:02 +0800

    Create planet_information.js

---
+++ static/js/planet_information.js
@@ -0,0 +1,12 @@
+import {
+  TOI_1231_B,
+  L_98_59_D,
+  GJ_1002_B,
+  PROXIMA_CENTAURI_B,
+  EPSILON_ERIDANI_B,
+  GLIESE_832_C,
+} from "./quiz_data.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bossCode = document.getElementById("bossCode");
+});
